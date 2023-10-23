8449c86f3a10394deb82d19c7701e188930e9f3c
Prakhar436 <personal.prakhar@gmail.com>
2023-10-23 16:09:19 +0530

    Add files via upload

---
+++ Program's_Contributed_By_Contributors/Java_Programs/Data_structure/Hashset.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+
+/**
+ * Hashset
+ */
+public class Hashset {
+
+    public static void main(String[] args) {
+        // HashSet<Integer> hs = new HashSet<>();
+        // HashSet<Integer> set = new HashSet<>();
+        // set.add(1);
+        // System.out.println(hs.add(1));
+        // System.out.println(hs.add(1));
+        // System.out.println(hs.equals(set));
+        // String s="g";
+        // s+="g";
+        // System.out.println(System.identityHashCode(s));
+        
+
+
+String arr[]={"a","b"};
+
+Class a=arr.getClass();
+
+String name=a.getName();
+
+System.out.println(arr.getClass());
+
+
+
+
+    }
+}
